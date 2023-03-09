dd1459f5ec9cdb8e90ca62a987fe5332e34da905
Lucas Kincaid <kincluc15@gmail.com>
2023-03-09 07:22:48 -0600

    Got the keyWord to interact with the spoonacular api

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 // Use querySelector to select search-bar and make it into a variable labeled searchButton
 var tableBody = document.getElementById('repo-table');
+var recpBody = document.getElementById('recp-table');
 var searchButton = document.querySelector('#search-button');
 var searchBar = document.querySelector('#search-bar');
 var keyWord = document.querySelector('#search-bar').value;
@@ -61,33 +62,68 @@ function getApi() {
     }
 
     // Adds event listener to retrieve getApi function
-    searchBar.addEventListener('click', getApi); 
+    searchButton.addEventListener('click', getApi); 
 
     // Function to retrieve recipes by ingredients using spoontacular API
     function getRecipeIngr() {
         console.log(keyWord);
         const options = {
             method: 'GET',
-            params: {
-                ingredients: 'ginger',
-                number: '5',
-                ignorePantry: 'false',
-            },
             headers: {
                 'X-RapidAPI-Key': '330bf6c932msh6e1ac73e9c79e8dp1dc19fjsn2174e9f05343',
                 'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
             }
         };
         
-        fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=herbs&ingredients=plants&ingredients=vegetables&diet=vegetarian&number=30&ignorePantry=true&ranking=1', options)
+        fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=${keyWord}&diet=vegetarian&number=30&ignorePantry=true&ranking=1`, options)
             .then(response => response.json())
-            .then(response => console.log(response))
+            .then((response) => {
+                console.log(response);
+                for (var i = 0; i < response.length; i++) {
+
+                    // Creating elements, tablerow, tabledata, and anchor
+                    var createTableRow = document.createElement('tr');
+                    var tableData = document.createElement('td');
+                    var dish = document.createElement('a');
+                
+                    // Setting the text of dish while making sure that no dishes are repeated
+                    if (i > 0) {
+                        var count = 0;
+                        // Checking to see if the dish's name has been previously appended
+                        for (var j = 1; j < i + 1; j++) {
+                            if (response[i].title !== response[j - 1].title) {
+                                count++;
+                            }
+                        }
+                        if (count === (i)) {
+                            dish.textContent = response[i].title;
+                            console.log(response[i].title);
+        
+                            // Appending the link to the tabledata and then appending the tabledata to the tablerow
+                            // The tablerow then gets appended to the tablebody
+                            tableData.appendChild(dish);
+                            createTableRow.appendChild(tableData);
+                            recpBody.appendChild(createTableRow);
+                        }
+                    }
+                    else {
+                        dish.textContent = response[i].title;
+                        console.log(response[i].title);
+        
+                        // Appending the link to the tabledata and then appending the tabledata to the tablerow
+                        // The tablerow then gets appended to the tablebody
+                        tableData.appendChild(dish);
+                        createTableRow.appendChild(tableData);
+                        recpBody.appendChild(createTableRow);
+                    }
+                 }
+            })
             .catch(err => console.error(err));
             
     }
 
     // connects previous made event listener to be able to retrieve data from getRecipeIngr function
-    searchBar.addEventListener('click', getRecipeIngr);
+    searchButton.addEventListener('click', getRecipeIngr);
 
     // Function to retrieve recipe information using spoontacular API, uses GET method. This will retrieve an object with 1 recipe and its data
     function getRecipeInfo() {
@@ -106,4 +142,4 @@ function getApi() {
             
     }
     // connects previous made event listener to be able to retrieve data from getRecipeInfo function
-    searchBar.addEventListener('click',  getRecipeInfo);
+    searchButton.addEventListener('click',  getRecipeInfo);
