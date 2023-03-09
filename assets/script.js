de51217dc1efb9a39aeb192c26b549a210691577
Lucas Kincaid <kincluc15@gmail.com>
2023-03-08 19:56:46 -0600

    Fixed Search bar and started work on append

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,13 @@
 // Use querySelector to select search-bar and make it into a variable labeled searchButton
-var searchButton = document.querySelector('#search-bar');
+var tableBody = document.getElementById('repo-table');
+var searchButton = document.querySelector('#search-button');
+var searchBar = document.querySelector('#search-bar');
+var keyWord = document.querySelector('#search-bar').value;
 
 // Function used to retrieve species-list information regarding the perrenual API
 function getApi() {
-    var requestUrl = ' https://perenual.com/api/species-list?page=1&key=sk-I07P640155763222b151&&edible=1';
+    keyWord = document.querySelector('#search-bar').value;
+    var requestUrl = `https://perenual.com/api/species-list?key=sk-I07P640155763222b151&q=${keyWord}&edible=1`;
 
     fetch(requestUrl)
     .then(function (response) {
@@ -11,17 +15,64 @@ function getApi() {
     })
     .then(function (data){
         console.log('data', data)
+        console.log(data.data.length);
+
+        for (var i = 0; i < data.data.length; i++) {
+
+            // Creating elements, tablerow, tabledata, and anchor
+            var createTableRow = document.createElement('tr');
+            var tableData = document.createElement('td');
+            var plant = document.createElement('a');
+        
+            // Setting the text of plant while making sure that no plants are repeated
+            if (i > 0) {
+                var count = 0;
+                // Checking to see if the plants name has been previously appended
+                for (var j = 1; j < i + 1; j++) {
+                    if (data.data[i].common_name !== data.data[j - 1].common_name) {
+                        count++;
+                    }
+                }
+                if (count === (i)) {
+                    plant.textContent = data.data[i].common_name;
+                    console.log(data.data[i].common_name);
+
+                    // Appending the link to the tabledata and then appending the tabledata to the tablerow
+                    // The tablerow then gets appended to the tablebody
+                    tableData.appendChild(plant);
+                    createTableRow.appendChild(tableData);
+                    tableBody.appendChild(createTableRow);
+                }
+            }
+            else {
+                plant.textContent = data.data[i].common_name;
+                console.log(data.data[i].common_name);
+
+                // Appending the link to the tabledata and then appending the tabledata to the tablerow
+                // The tablerow then gets appended to the tablebody
+                tableData.appendChild(plant);
+                createTableRow.appendChild(tableData);
+                tableBody.appendChild(createTableRow);
+            }
+        }
+        
     })
     
     }
 
     // Adds event listener to retrieve getApi function
-    searchButton.addEventListener('click', getApi); 
+    searchBar.addEventListener('click', getApi); 
 
     // Function to retrieve recipes by ingredients using spoontacular API
     function getRecipeIngr() {
+        console.log(keyWord);
         const options = {
             method: 'GET',
+            params: {
+                ingredients: 'ginger',
+                number: '5',
+                ignorePantry: 'false',
+            },
             headers: {
                 'X-RapidAPI-Key': '330bf6c932msh6e1ac73e9c79e8dp1dc19fjsn2174e9f05343',
                 'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
@@ -36,7 +87,7 @@ function getApi() {
     }
 
     // connects previous made event listener to be able to retrieve data from getRecipeIngr function
-    searchButton.addEventListener('click', getRecipeIngr);
+    searchBar.addEventListener('click', getRecipeIngr);
 
     // Function to retrieve recipe information using spoontacular API, uses GET method. This will retrieve an object with 1 recipe and its data
     function getRecipeInfo() {
@@ -55,4 +106,4 @@ function getApi() {
             
     }
     // connects previous made event listener to be able to retrieve data from getRecipeInfo function
-    searchButton.addEventListener('click',  getRecipeInfo);
+    searchBar.addEventListener('click',  getRecipeInfo);
